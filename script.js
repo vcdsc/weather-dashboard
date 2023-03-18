5af16bf2bbbc44efd866465d64d463a2a9983bf4
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-03-18 21:22:34 +0000

    adjust bootstrap class in cards and add jumbotron
    cards now displaying as 5 columns in a row, with appropriate spacing between them; add jumbotron to display current weather

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ var OpenWeatherDateFormat = "YYYY-MM-DD HH:mm:ss";
 // Number of locations in response.
 var defaultLimit = 1;
 
+$("#search-button").addClass("btn-dark");
+
+// Will need this later to display city name in jumbotron.
+var displayLocation;
+
 function filterForecast(list) {
-  // Since the API call (https://openweathermap.org/forecast5) returns 5 days of results spread across 3 hour intervals, the very first result can be used as a guide. Hours are displayed in the 24 hour format, so these will not repeat. Following that logic, can filter all the results in the API response that have the same time as that first result, which will get me 5 days worth of weather forecast to display. Can achieve this by targeting the API response field that holds the date and time, `dt_txt`.
+  // Since the API call (https://openweathermap.org/forecast5) returns 5 days of results spread across 3 hour intervals, the very first result can be used as a guide. Hours are displayed in the 24 hour format, so these will not repeat. Following that logic, can filter all the results in the API response that have the same time as that first result, which will get me (on a best case scenario) 5 days worth of weather forecast to display. Can achieve this by targeting the API response field that holds the date and time, `dt_txt`.
   var initialForecastDate = moment(list[0].dt_txt, OpenWeatherDateFormat);
 
   var forecastDates = [];
@@ -19,16 +24,17 @@ function filterForecast(list) {
     );
   }
 
-  // If the dates saved in `forecastDates` are found in the filter that runs through the API response (list), then save the corresponding weather object into the result array.
+  // If the dates saved in the `forecastDates` array are found in the filter that runs through the API response (list), then save the corresponding weather object into the `result` array.
   var result = list.filter(function (forecast) {
     return forecastDates.includes(forecast.dt_txt);
   });
 
   if (result.length < 6) {
+    // In the event that the hour we are using as a guide does not yeld 6 (daily and 5 day forecast) results, go get the last available result(s) to complete the `result` array.
     result.push(list[list.length - 1]);
   }
 
-  console.log("result (6 day forecast, all response fields) ===>", result);
+  // console.log("result (6 day forecast, all response fields) ===>", result);
   return result;
 }
 
@@ -46,24 +52,41 @@ function forecastDetails(list) {
   });
 }
 
-// Populating the cards with our response data.
+// Populating the jumbotron with current weather forecast.
+function jumbotronElement(element, contents) {
+  $(element).append(`
+  <div class="jumbotron">
+    <h1 class="display-6"> (${contents.date})<img src="${contents.icon}" /></h1>
+      <br>
+        <p>Temperature: ${contents.temperature} °C</p>
+        <p>Wind: ${contents.wind} KPH</p>
+        <p>Humidity: ${contents.humidity} %</p>
+  </div>
+  `);
+
+  $(".display-6").prepend(`${displayLocation}`);
+}
+
+// Populating the cards with the 5 day forecast.
 function cardElement(element, contents) {
-  $(element).append(
-    `<div class="card bg-secondary text-light" style="width: 18rem;">
-        <div class="card-body">
-            <h5 class="card-title">${contents.date}</h5>
-                <img src="${contents.icon}" />
-                <p>Temperature: ${contents.temperature} °C</p>
-                <p>Wind: ${contents.wind} KPH</p>
-                <p>Humidity: ${contents.humidity} %</p>
-        </div>
-    </div>`
-  );
+  $(element).append(`
+  <div class="col">
+    <div class="card bg-secondary text-light">
+      <div class="card-body">
+        <h6 class="card-title">${contents.date}<img src="${contents.icon}" /></h6>
+          <p>Temperature: ${contents.temperature} °C</p>
+          <p>Wind: ${contents.wind} KPH</p>
+          <p>Humidity: ${contents.humidity} %</p>
+      </div>
+    </div>
+  </div>
+    `);
 }
 
 function dashboard(location) {
   // If the user simply hits "Enter" or clicks "Search" (without entering any text) they need to be prompted to complete the necessary field.
   if (location === "") {
+    // !!! Currently only logging in browser console.
     throw new Error("Sorry, city name cannot be empty. Care to try again?");
   }
 
@@ -72,7 +95,7 @@ function dashboard(location) {
     url: `http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=${defaultLimit}&appid=${OpenWeatherAPIKey}`,
     method: "GET",
   }).then(function (coordinatesResponse) {
-    console.log("coordinatesResponse ===>", coordinatesResponse);
+    // console.log("coordinatesResponse ===>", coordinatesResponse);
 
     // If the user types an invalid location, they need to be prompted to re-type the city name. We know this will be an invalid location when our coordinatesResponse comes back as an empty array.
     if (!coordinatesResponse.length) {
@@ -91,13 +114,28 @@ function dashboard(location) {
         url: `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${OpenWeatherAPIKey}&units=metric`,
         method: "GET",
       }).then(function (forecastResponse) {
+        // console.log(
+        //   "forecastResponse ===>",
+        //   forecastResponse);
+
+        displayLocation = forecastResponse.city.name;
+
         var filteredForecast = filterForecast(forecastResponse.list);
-        console.log("filtered list ===>", filteredForecast);
+        // console.log("filtered list ===>", filteredForecast);
 
         var details = forecastDetails(filteredForecast);
+        // console.log("details ===>", details);
+
+        $("#forecast").append(`
+        <div class="container">
+          <h2>5 Day Forecast</h2>
+            <div class="row">
+            </div>
+        </div>
+          `);
 
         // display current weather forecast
-        cardElement("#today", details[0]);
+        jumbotronElement("#today", details[0]);
 
         // display 5 day weather forecast
         for (let i = 1; i < 6; i++) {
